53855faf55471017106008a99a8cdb7c527bfb9d
Hadirehman <hadirehman69@gmail.com>
2022-02-27 10:14:51 -0800

    best pizza project
    pizza is my best

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/bowl1.png";
+import Margherita from "../assets/bowl2.png";
+import PedroTechSpecial from "../assets/bowl3.png";
+import Vegan from "../assets/bowl4.png";
+import Pineapple from "../assets/pizza.png";
+import Expensive from "../assets/fruits.png";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 15.99,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 11.99,
+  },
+  {
+    name: "PedroTech Special Pizza",
+    image: PedroTechSpecial,
+    price: 256.53,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 17.99,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 4.99,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 1997.99,
+  },
+];
